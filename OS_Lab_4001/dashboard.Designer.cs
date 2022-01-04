b6e01a39e8e5837dc7a09377a0f6266cf52d92c0
marezayeye <marezayeye@gmail.com>
2022-01-04 19:01:32 +0330

    Add project files.

---
+++ OS_Lab_4001/dashboard.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace OS_Lab_4001
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "کتاب ها";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(206, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "اعضا";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "دسته بندی ها";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "امانات";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(250, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "خروج";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 201);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "dashboard";
+            this.Text = "داشبورد";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ OS_Lab_4001/login_form.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OS_Lab_4001
+{
+    public partial class login_form : Form
+    {
+        private SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public login_form()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Data Source=.;Initial Catalog=Library_DB;Integrated Security=True");
+        }
+       
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * From tblAdmin where aUser ='"+textBox1.Text+"' And aPass = '"+textBox2.Text +"'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("ورود موفقیت آمیز بود");
+                //
+                //
+                //
+                this.Visible = false;     // method2
+                dashboard s = new dashboard();  
+                s.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("نام کاربری یا رمز عبور اشتباه است");
+            }
+            con.Close();
+         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
