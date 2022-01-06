a53f539226db7e5a0616c36a4547743925bea47a
marezayeye <marezayeye@gmail.com>
2022-01-06 22:47:40 +0330

    Created Sub Forms for new lend and returned lends @lend_form

---
+++ OS_Lab_4001/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS_Lab_4001
+{
+    public partial class new_lend_form : Form
+    {
+        public new_lend_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
