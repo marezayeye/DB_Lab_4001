810b57753190a996032b36b3ab0c1533d1f1ed0e
marezayeye <marezayeye@gmail.com>
2022-01-06 21:50:36 +0330

    Created Lend_form

---
+++ OS_Lab_4001/lend_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS_Lab_4001
+{
+    public partial class lend_form : Form
+    {
+        public lend_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
