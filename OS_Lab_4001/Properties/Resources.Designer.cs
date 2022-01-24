c33b735f640f149f3e2e5e09ece102e2879ab7f0
marezayeye <marezayeye@gmail.com>
2022-01-25 03:22:59 +0330

    Updated Visuals @dashboard

--- OS_Lab_4001/Properties/Resources.Designer.cs
+++ OS_Lab_4001/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace OS_Lab_4001.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap dashboard_bg {
+            get {
+                object obj = ResourceManager.GetObject("dashboard-bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
