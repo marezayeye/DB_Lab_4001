955a95dcdbbf9081462c4d98b0f32c064e8d7bae
marezayeye <marezayeye@gmail.com>
2022-01-27 03:26:06 +0330

    Updated Visuals @lend_return implemented return funcions (test needed)

--- OS_Lab_4001/lend_return.Designer.cs
+++ OS_Lab_4001/lend_return.Designer.cs
@@ -44,91 +44,110 @@ namespace OS_Lab_4001
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(107, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 17);
+            this.label1.Size = new System.Drawing.Size(213, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "برای بررسی لطفا کد امانت را وارد کنید";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(252, 155);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new System.Drawing.Size(58, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "نام کتاب";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(248, 191);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(255, 191);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.Size = new System.Drawing.Size(55, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "نام کاربر";
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.CadetBlue;
+            this.button1.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 48);
             this.button1.TabIndex = 3;
             this.button1.Text = "بلی";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(173, 361);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 48);
             this.button2.TabIndex = 4;
             this.button2.Text = "خیر";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(152, 318);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 316);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(147, 17);
+            this.label4.Size = new System.Drawing.Size(176, 23);
             this.label4.TabIndex = 5;
             this.label4.Text = "آیا اطلاعات فوق را تایید میکنید";
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(36, 52);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(182, 22);
+            this.textBox1.Size = new System.Drawing.Size(262, 28);
             this.textBox1.TabIndex = 6;
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.CadetBlue;
+            this.button3.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(36, 80);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(262, 44);
             this.button3.TabIndex = 7;
             this.button3.Text = "بررسی";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(36, 155);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.Size = new System.Drawing.Size(47, 23);
             this.label5.TabIndex = 8;
             this.label5.Text = "label5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(36, 191);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.Size = new System.Drawing.Size(47, 23);
             this.label6.TabIndex = 9;
             this.label6.Text = "label6";
             // 
@@ -136,6 +155,7 @@ namespace OS_Lab_4001
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OS_Lab_4001.Properties.Resources._39_Create_a_Login_Form_in_Adobe_Photoshop_From_Scratch;
             this.ClientSize = new System.Drawing.Size(329, 425);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
--- OS_Lab_4001/lend_return.cs
+++ OS_Lab_4001/lend_return.cs
@@ -22,6 +22,8 @@ namespace OS_Lab_4001
         SqlConnection lr_con = new SqlConnection("Data Source =.; Initial Catalog = Library_DB; Integrated Security = True");
         SqlCommand lr_cmd = new SqlCommand();
         SqlDataReader lr_dr;
+        public static int lr_book_id;
+        public static int lr_user_id;
         private void lend_return_Load(object sender, EventArgs e)
         {
             button1.Visible = false;
@@ -52,6 +54,42 @@ namespace OS_Lab_4001
                 label6.Visible = true;
                 label4.Visible = true;
                 lr_con.Close();
+
+                
+                //finding book id and user id
+                lr_con.Open();
+                SqlDataAdapter lr_da = new SqlDataAdapter("Select lBook, lUser From tblLend Where lId = '" + textBox1.Text + "'", lr_con);
+                DataTable lr_dt = new DataTable();
+                lr_da.Fill(lr_dt);
+                DataRow row = lr_dt.Rows[0];
+                lr_book_id = Convert.ToInt32(row["lBook"].ToString());
+                lr_user_id = Convert.ToInt32(row["lUser"].ToString());
+                lr_con.Close();
+
+                //finding book name
+                lr_con.Open();
+                SqlDataAdapter lr_da1 = new SqlDataAdapter ("Select bName From tblBook Where bID = '" + lr_book_id + "'", lr_con);
+                DataTable lr_dt1 = new DataTable();
+                lr_da1.Fill(lr_dt1);
+                DataRow row1 = lr_dt1.Rows[0];
+                string lr_book_name = row1["bName"].ToString();
+                lr_con.Close();
+
+                //finding user name 
+                lr_con.Open();
+                SqlDataAdapter lr_da2 = new SqlDataAdapter("Select uFirstname, uLastname From tblUser Where uId = '" + lr_user_id + "'", lr_con);
+                DataTable lr_dt2 = new DataTable();
+                lr_da2.Fill(lr_dt2);
+                DataRow row2 = lr_dt2.Rows[0];
+                string lr_firstname = row2["uFirstname"].ToString();
+                string lr_lastname = row2["uLastname"].ToString();
+                string lr_name = lr_firstname + " " + lr_lastname;
+
+
+                //setting labels
+                label5.Text = lr_book_name;
+                label6.Text = lr_name;
+
             }
             else 
             {
@@ -67,15 +105,34 @@ namespace OS_Lab_4001
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //getting book id 
-
-            //getting user id
 
             //runnig tblLend query
-            
-            //running tblUser query
+            using (var lr_con1 = new SqlConnection("Data Source=.;Initial Catalog=Library_DB;Integrated Security=True"))
+            {
+                lr_con1.Open();
 
-            //running tblBook query
+                using (var command = lr_con1.CreateCommand())
+                {
+                    //executing query on tblLends
+                    command.CommandText = "UPDATE tblLend SET lReturned = 1 WHERE lId = @lid;";
+                    command.Parameters.Add(new SqlParameter("lid",textBox1.Text ));
+                    command.ExecuteNonQuery();
+
+                    //executing query on tblUser
+                    command.CommandText = "UPDATE tblUser SET uBorrowedcount = uBorrowedcount -1 1 WHERE uId = @uid;";
+                    command.Parameters.Add(new SqlParameter("uid", lr_user_id));
+                    command.ExecuteNonQuery();
+
+                    //executing query on tblBook
+                    command.CommandText = "UPDATE tblBook SET bBorrowd = 0 WHERE bID = @bid;";
+                    command.Parameters.Add(new SqlParameter("bid1", lr_book_id));
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("برگشت امانت با موفقیت ثبت شد");
+            this.Hide();
+
         }
     }
 }
