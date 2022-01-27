c112b8f131d9b571ad322d8958e780fcd840276c
marezayeye <marezayeye@gmail.com>
2022-01-27 03:30:26 +0330

    Added Button to open lend_return form Removed old functions for returning lend

--- OS_Lab_4001/lend_form.cs
+++ OS_Lab_4001/lend_form.cs
@@ -32,41 +32,8 @@ namespace OS_Lab_4001
 
         private void button4_Click(object sender, EventArgs e)
         {
-            DialogResult dialog = MessageBox.Show("آیا از ثبت بازگشت مطمئن هستید؟","ثبت بازگشت امانت", MessageBoxButtons.YesNo);
-            if (dialog == DialogResult.Yes)
-            {
-                int return_lend_id = Convert.ToInt32(textBox1.Text);
-                int return_book_id = Convert.ToInt32(textBox2.Text);
-                int return_user_id = Convert.ToInt32(textBox3.Text);
-
-                using (var return_con = new SqlConnection("Data Source=.;Initial Catalog=Library_DB;Integrated Security=True"))
-                {
-                    return_con.Open();
-
-                    using (var command = return_con.CreateCommand())
-                    {
-                        command.CommandText = "UPDATE  tblLend SET lReturned = 1 WHERE lId = @lid;";
-                        command.Parameters.Add(new SqlParameter("lid", return_lend_id));
-                        command.ExecuteNonQuery();
-
-                        command.CommandText = "UPDATE tblUser SET uBorrowedcount = uBorrowedcount + -1 WHERE uId = @uid;";
-                        command.Parameters.Add(new SqlParameter("uid", return_user_id));
-                        command.ExecuteNonQuery();
-
-                        command.CommandText = "UPDATE tblBook SET bBorrowd = 0 WHERE bID = @bid;";
-                        command.Parameters.Add(new SqlParameter("bid", return_book_id));
-                        command.ExecuteNonQuery();
-                    }
-                }
-                MessageBox.Show("بازگشت کتاب با موفقیت ثبت شد");
-                lend_dg.Refresh();
-            }
-            else
-            {
-                MessageBox.Show("عملیات به درخواست کاربر لغو شد");
-            }
-            
-            
+            lend_return lr = new lend_return();
+            lr.Visible = true;
 
         }
 
@@ -129,73 +96,16 @@ namespace OS_Lab_4001
 
         private void lend_dg_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            int lend_id = Convert.ToInt32(lend_dg.SelectedCells[0].Value.ToString());
-            try
-            {
-                lend_cmd = new SqlCommand();
-                lend_con.Open();
-                lend_cmd.Connection = lend_con;
-                lend_cmd.CommandType = CommandType.Text;
-                lend_cmd.CommandText = "SELECT * FROM tblLend WHERE lId =" + lend_id + "";
-                lend_cmd.ExecuteNonQuery();
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(lend_cmd);
-                da.Fill(dt);
-
-                foreach (DataRow dr in dt.Rows)
-                {
-
-                    textBox1.Text = dr["lId"].ToString();
-                    textBox2.Text = dr["lBook"].ToString();
-                    textBox3.Text = dr["lUser"].ToString();
-                }
-                lend_con.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            
         }
 
         private void lend_dg_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int lend_id = Convert.ToInt32(lend_dg.SelectedCells[0].Value.ToString());
-            try
-            {
-                lend_cmd = new SqlCommand();
-                lend_con.Open();
-                lend_cmd.Connection = lend_con;
-                lend_cmd.CommandType = CommandType.Text;
-                lend_cmd.CommandText = "SELECT * FROM tblLend WHERE lId =" + lend_id + "";
-                lend_cmd.ExecuteNonQuery();
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(lend_cmd);
-                da.Fill(dt);
-
-                foreach (DataRow dr in dt.Rows)
-                {
-                    
-                    textBox1.Text = dr["lLend_id"].ToString();
-                    textBox2.Text = dr["lBook_id"].ToString();
-                    textBox3.Text = dr["lUser"].ToString();
-                }
-                lend_con.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            
         }
         private void lend_dg_SelectionChanged(object sender, EventArgs e)
         {
-            if (lend_dg.SelectedCells.Count > 0)
-            {
-                int selectedrowindex = lend_dg.SelectedCells[0].RowIndex;
-                DataGridViewRow selectedRow = lend_dg.Rows[selectedrowindex];
-                textBox1.Text = Convert.ToString(selectedRow.Cells["lLend_id"].Value);
-                textBox2.Text = Convert.ToString(selectedRow.Cells["lBook_id"].Value);
-                textBox3.Text = Convert.ToString(selectedRow.Cells["lUser"].Value);
-            }
+            
         }
     }
 }
